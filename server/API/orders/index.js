2ab9feab90a73625f3f0f1d5cc9409e394f2236a
sushma82 <sushmareddy8290@gmail.com>
2021-10-03 19:22:16 +0530

    add passport jwt private route config

--- server/API/orders/index.js
+++ server/API/orders/index.js
@@ -14,21 +14,26 @@ Params    _id
 Access    Public
 Method    GET  
 */
-Router.get("/:_id", async (req, res) => {
-  try {
-    const { _id } = req.params;
 
-    const getOrders = await OrderModel.findOne({ user: _id });
+Router.get(
+    "/:_id",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      try {
+        const { _id } = req.params;
 
-    if (!getOrders) {
-      return res.status(404).json({ error: "User not found" });
+      const getOrders = await OrderModel.findOne({ user: _id });
+
+      if (!getOrders) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      return res.status(200).json({ orders: getOrders });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
     }
-
-    return res.status(200).json({ orders: getOrders });
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
   }
-});
+  );
 
 /*
 Route     /new
