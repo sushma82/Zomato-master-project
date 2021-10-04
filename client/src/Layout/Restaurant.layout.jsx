c664716cf678392f2755f8ec3b0928cb7615180f
sushma82 <sushmareddy8290@gmail.com>
2021-10-04 22:31:28 +0530

    add restaurant layout

--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -1,22 +1,51 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { TiStarOutline } from "react-icons/ti";
+import { RiDirectionLine, RiShareForwardLine } from "react-icons/ri";
+import { BiBookmarkPlus } from "react-icons/bi";
 
-// Layout
-import RestaurantLayout from "../Layout/Restaurant.layout";
+// components
+import RestaurantNavbar from "../Components/Navbar/restaurantNavbar";
+import ImageGrid from "../Components/restaurant/ImageGrid";
+import InfoButtons from "../Components/restaurant/InfoButtons";
+import RestaurantInfo from "../Components/restaurant/RestaurantInfo";
 
-const RestaurantLayoutHOC = ({ component: Component, ...rest }) => {
+const RestaurantLayout = () => {
   return (
     <>
-      <Route
-        {...rest}
-        component={(props) => (
-          <RestaurantLayout>
-            <Component {...props} />
-          </RestaurantLayout>
-        )}
-      />
+      {" "}
+      <RestaurantNavbar />
+      <div className="container mx-auto px-4 lg:px-20 ">
+        <ImageGrid
+          images={[
+            "https://b.zmtcdn.com/data/pictures/2/18621252/f737723f080910e46c451c51b9bbd717.jpg?output-format=webp",
+            "https://b.zmtcdn.com/data/pictures/2/18621252/f737723f080910e46c451c51b9bbd717.jpg?output-format=webp",
+            "https://b.zmtcdn.com/data/pictures/2/18621252/f737723f080910e46c451c51b9bbd717.jpg?output-format=webp",
+            "https://b.zmtcdn.com/data/pictures/2/18621252/f737723f080910e46c451c51b9bbd717.jpg?output-format=webp",
+          ]}
+        />
+        <RestaurantInfo
+          name="Mumbai Xpress"
+          restaurantRating="3.5"
+          deliveryRating="3.2"
+          cuisine="North Indian, Fast Food, Chinese, Street Food"
+          address="Basavanagudi, Bangalore"
+        />
+        <div className="my-4 flex flex-wrap gap-3">
+          <InfoButtons isActive>
+            <TiStarOutline /> Add Review
+          </InfoButtons>
+          <InfoButtons>
+            <RiDirectionLine /> Direction
+          </InfoButtons>
+          <InfoButtons>
+            <BiBookmarkPlus /> Bookmark
+          </InfoButtons>
+          <InfoButtons>
+            <RiShareForwardLine /> Share
+          </InfoButtons>
+        </div>
+      </div>
     </>
   );
 };
-
-export default RestaurantLayoutHOC;
+export default RestaurantLayout;
